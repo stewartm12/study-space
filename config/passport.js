ab3068216cf2558683e1450ae57f346b334d50c0
Stewart Morales <stewartm12@github.com>
2021-12-13 13:12:57 -0500

    Configure passport

--- config/passport.js
+++ config/passport.js
@@ -1,19 +1,26 @@
-// We want to use strategy to handle josn web tokens
-const JwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 const keys = require("./keys");
 
 const options = {};
-options.JwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 options.secretOrKey = keys.secretOrKey;
 
 module.exports = (passport) => {
   passport.use(
     new JwtStrategy(options, (jwt_payload, done) => {
-      console.log(jwt_payload);
-      done();
+      User.findById(jwt_payload.id)
+        .then((user) => {
+          if (user) {
+            // return the user to the frontend
+            return done(null, user);
+          }
+          // return false since there is no user
+          return done(null, false);
+        })
+        .catch((err) => console.log(err));
     })
   );
 };
