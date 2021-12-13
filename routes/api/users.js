d95dedf37c76476bff9d71534ecb2602adaba9b8
Stewart Morales <stewartm12@github.com>
2021-12-13 13:15:01 -0500

    Passport authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 const {
   getUsers,
   registerUsers,
   loginUser,
 } = require("../../controller/users");
 
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      username: req.user.username,
+      email: req.user.email,
+      date: req.user.date,
+    });
+  }
+);
+
 router.get("/", getUsers);
 
 router.post("/register", registerUsers);
 
 router.post("/login", loginUser);
+
+router.get(
+  "/logout",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      username: req.user.username,
+      email: req.user.email,
+    });
+  }
+);
 
 module.exports = router;
